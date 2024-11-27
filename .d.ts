4b89a58249339188111bd1c39c6887bfe264b93a
HainerSt <hainer.stefan@gmail.com>
2024-11-27 21:51:27 +0200

    bug fix

---
+++ .d.ts
@@ -0,0 +1,4 @@
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+  const flattenColorPalette: (palette: Record<string, any>) => Record<string, string>;
+  export default flattenColorPalette;
+}
